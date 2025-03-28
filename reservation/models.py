6a09734b9ea53ae9a97060c2329391fcbf0cc687
Daniel <callejasmark63@gmail.com>
2025-03-28 21:36:44 +0800

    ❄️ Added models, add initial data

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.SmallIntegerField()
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name + ' , ' + str(self.no_of_guest)+ ' guesses' + ' , ' + str(self.booking_date)
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10 , decimal_places=2)
+    inventory = models.BooleanField()
+
+    def __str__(self):
+        return 'name: ' + self.title + ', price: ' + str(self.price) + ', stock: ' + str(self.inventory)

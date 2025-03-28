24c3586ea25fff40cad2f03480808afc7bb8b62a
Daniel <callejasmark63@gmail.com>
2025-03-28 22:22:17 +0800

    Added views

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('menu/', views.BookingView.as_view()),
+    path('booking/', views.MenuView.as_view()),
+    path('api/menu/', views.MenuItemView.as_view()),
+    path('api/menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api/booking/', views.BookingItemView.as_view()),
+    path('api/booking/<int:pk>', views.SingleBookingItemView.as_view()),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# * DRF Stuffs
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+# * Models stuffs
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
 def home(request):
     return render(request, 'index.html', {})
+
+#   API VIEW
+class BookingView(APIView):
+
+    def get(self, request):
+        items = Booking.objects.all()   # queryset
+        serializer = BookingSerializer(items, many= True) # model calss, serializer class
+        return Response(serializer.data)
+
+class MenuView(APIView):
+
+    def get(self, request):
+        items = Menu.objects.all() 
+        serializer = MenuSerializer(items, many= True) # model calss, serializer class
+        return Response(serializer.data)
+
+# Generics
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    # permission_classes = [permission.IsAuthenticated]
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingItemView(ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class SingleBookingItemView(RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
